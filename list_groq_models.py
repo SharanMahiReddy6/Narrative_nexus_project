c7edcdc1dbfb0697fdf6933fe83b81222bec85f4
SharanMahiReddy6 <mandatisharanmahireddy@gmail.com>
2025-10-29 20:07:28 +0530

    Add project files

--- list_groq_models.py
+++ list_groq_models.py
@@ -1,4 +1,4 @@
 from groq import Groq
-client = Groq(api_key="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")
+client = Groq(api_key="YOUR_GROQ_API_KEY")
 response = client.models.list()
 print([m.id for m in response.data])
